70d1eda5a71f5cb9ca04be44d7d16ab4d76c6858
malasbelajarr-a11y <malasbelajarr@gmail.com>
2026-09-06 00:16:36 +0700

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -2,16 +2,16 @@ export async function POST(req: Request) {
   const body = await req.json()
   const { name, email, password, code } = body
 
-  const KODE_MEMBER = ["MLS-NGU-2026", "MLS-MAN-2026"]
-  const KODE_MENTOR = "CEKEKOKOMLS" // <-- PAKAI YG INI YA
+  const KODE_MEMBER = ["MLS-NGU-2026"]
+  const KODE_MENTOR = "CEKEKOKOMLS"
 
   if (!KODE_MEMBER.includes(code) && code !== KODE_MENTOR) {
-    return Response.json({ error: "Kode salah" }, { status: 400 })
+    return Response.json({ error: "Kode mentor tidak cocok" }, { status: 400 })
   }
 
   if (code === KODE_MENTOR) {
-    return Response.json({ message: "Daftar berhasil! Kamu MENTOR" }, { status: 200 })
+    return Response.json({ message: "Verifikasi berhasil! Selamat datang Mentor" }, { status: 200 })
   } else {
-    return Response.json({ message: "Daftar berhasil! Kamu MEMBER" }, { status: 200 })
+    return Response.json({ message: "Daftar berhasil! Selamat datang Member" }, { status: 200 })
   }
 }
