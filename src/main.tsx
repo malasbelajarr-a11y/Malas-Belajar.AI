503d901f3de9e07c22e693829b20b097d3c5fb8d
malasbelajarr-a11y <malasbelajarr@gmail.com>
2026-09-06 11:38:14 +0700

    apply inline UTBABY result and explanation layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "./tryout-result.css";
 import App from "./App.tsx";
 import { queryClient } from "./lib/queryClient";
 
