7fb072dc759ce6f745380fcfd0d8b33b6fc99db7
malasbelajarr-a11y <malasbelajarr@gmail.com>
2026-09-07 16:43:40 +0700

    Gate student app behind mandatory mentor access code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
+import StudentAuthGate from "@/components/StudentAuthGate";
 import { Toaster } from "sonner";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <StudentAuthGate>
+              <Home />
+            </StudentAuthGate>
+          }
+        />
       </Routes>
       <Toaster richColors position="top-right" />
     </>
