16892dbad05acb2d4be7d8b2b21d578754b541da
malasbelajarr-a11y <malasbelajarr@gmail.com>
2026-09-08 19:42:32 +0700

    Add dedicated student account and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
+import StudentAuth from "@/pages/StudentAuth";
 import AuthVisualFix from "@/components/AuthVisualFix";
 import { Toaster } from "sonner";
 
@@ -7,14 +8,9 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <AuthVisualFix>
-              <Home />
-            </AuthVisualFix>
-          }
-        />
+        <Route path="/" element={<AuthVisualFix><Home /></AuthVisualFix>} />
+        <Route path="/akun" element={<StudentAuth />} />
+        <Route path="/login" element={<StudentAuth />} />
       </Routes>
       <Toaster richColors position="top-right" />
     </>
