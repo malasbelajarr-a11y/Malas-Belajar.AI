a22b571a3ef2a664ec97a7abd7dfbbfa865381cf
malasbelajarr-a11y <malasbelajarr@gmail.com>
2026-09-15 12:26:02 +0700

    fix: restore Wacawaci locker UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AuthVisualFix from "@/components/AuthVisualFix";
 import HideOldMentorFix from "@/components/HideOldMentorFix";
 import WacawaciUploadFix from "@/components/WacawaciUploadFix";
 import MentorContentManagerFix from "@/components/MentorContentManagerFix";
+import LockerSubtestFix from "@/components/LockerSubtestFix";
 import StudentEntryGate from "@/components/StudentEntryGate";
 import { Toaster } from "sonner";
 
@@ -12,7 +13,6 @@ export default function App() {
   return (
     <>
       <Routes>
-        {/* Home tetap utuh supaya portal dan akses mentor asli tidak hilang. */}
         <Route path="/" element={<AuthVisualFix><Home /></AuthVisualFix>} />
         <Route path="/akun" element={<StudentAuth />} />
         <Route path="/login" element={<StudentAuth />} />
@@ -21,6 +21,7 @@ export default function App() {
       <HideOldMentorFix />
       <WacawaciUploadFix />
       <MentorContentManagerFix />
+      <LockerSubtestFix />
       <Toaster richColors position="top-right" />
     </>
   );
