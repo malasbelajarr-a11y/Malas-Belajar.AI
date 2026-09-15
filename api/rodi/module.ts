83ebdb42bfcbe7e77d35947bd76ea1e37658f195
malasbelajarr-a11y <malasbelajarr@gmail.com>
2026-09-16 01:10:49 +0700

    Merge mentor questions into RODI module

---
+++ api/rodi/module.ts
@@ -0,0 +1,8 @@
+import app from "../../server";
+import { mentorQuestions } from "../_lib/mentorBank";
+export default async function handler(req:any,res:any){
+ if(req.method!=="GET")return res.status(405).json({detail:"Method tidak didukung."});
+ const originalJson=res.json.bind(res);
+ res.json=(data:any)=>{if(data&&Array.isArray(data.questions)){data.questions=[...mentorQuestions,...data.questions];data.total_questions=data.questions.length}return originalJson(data)};
+ return app(req,res);
+}
